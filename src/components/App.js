39d42513fc15a230dbe6159fbd1349246e780827
Olha Hizhytska <olgainfotech@gmail.com>
2021-04-17 12:54:50 -0700

    update app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,15 @@
 import React from "react";
 import Header from "./Header";
+import TicketList from "./TicketList";
 
 function App(){
-  const name = "Thato";
-  const name2 = "Haley";
-  return (
+  return ( 
     <React.Fragment>
       <Header />
-      <h3>3a</h3>
-      <h3>{name} and {name2}</h3>
-      <p><em>Firebase entries not saving!</em></p>
-      <hr/>
+      <TicketList />
     </React.Fragment>
   );
 }
 
 export default App;
-
 // React is using React.createElement() to create each HTML element
